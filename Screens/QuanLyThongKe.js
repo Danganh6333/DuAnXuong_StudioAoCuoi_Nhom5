a0466a25bd027cf60109b25f8bbd137fd068e8fe
Admin <danganh6785@gmail.com>
2024-03-24 14:14:03 +0700

    Init

---
+++ Screens/QuanLyThongKe.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const QuanLyThongKe = () => {
+  return (
+    <View>
+      <Text>QuanLyThongKe</Text>
+    </View>
+  )
+}
+
+export default QuanLyThongKe
+
+const styles = StyleSheet.create({})
