3d504108379162ee1052e0a6985a80cbe7820e9a
Admin <danganh6785@gmail.com>
2024-03-30 06:44:40 +0700

    aaaaaaaaaa

--- Screens/QuanLyHoaDon.js
+++ Screens/QuanLyHoaDon.js
@@ -5,36 +5,48 @@ import {
   Modal,
   StyleSheet,
   Text,
+  TextInput,
   View,
+  StatusBar,
+  TouchableOpacity,
 } from 'react-native';
 import React, {useEffect, useState} from 'react';
-import {Checkbox} from 'react-native-paper';
-import {StatusBar} from 'react-native';
-import {TouchableOpacity} from 'react-native';
-import COMMON from '../COMMON';
 import {AnimatedFAB} from 'react-native-paper';
-import { Picker } from '@react-native-picker/picker';
+import {Picker} from '@react-native-picker/picker';
+import moment from 'moment';
+import COMMON from '../COMMON';
 
 const QuanLyHoaDon = () => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [khachHangData, setKhachHangData] = useState([]);
-  const [khachHangHoTen, setKhachHangHoTen] = useState('')
+  const [khachHangHoTen, setKhachHangHoTen] = useState('');
   const [nhanVienData, setNhanVienData] = useState([]);
   const [nhanVienHoTen, setNhanVienHoTen] = useState('');
   const [showAddDialog, setShowAddDialog] = useState(false);
-  const [isExtended, setIsExtended] = React.useState(true);
+  const [isExtended, setIsExtended] = useState(true);
+  const [currentDate, setCurrentDate] = useState('');
+  const [tenKhachHang, setTenKhachHang] = useState('');
+  const [idNhanVien, setIdNhanVien] = useState('');
+  const [idKhachHang, setIdKhachHang] = useState('');
+  const [tongTien, setTongTien] = useState('');
+
+  const getCurrentDate = () => {
+    var date = moment().utcOffset('+07:00').format('YYYY-MM-DD hh:mm:ss a');
+    setCurrentDate(date);
+  };
 
   const onScroll = ({nativeEvent}) => {
     const currentScrollPosition =
       Math.floor(nativeEvent?.contentOffset?.y) ?? 0;
     setIsExtended(currentScrollPosition <= 0);
   };
+
   const toggleAddDialog = value => {
     setShowAddDialog(value);
   };
 
-  const fabStyle = {right: 16, bottom: 16};
+  const fabStyle = {right: 10, bottom: 2};
 
   const getSpinnerKhachHang = async () => {
     try {
@@ -42,7 +54,6 @@ const QuanLyHoaDon = () => {
         `http://${COMMON.ipv4}:3000/khachhangs/getKhachHang`,
       );
       const json = await response.json();
-      console.log(json);
       setKhachHangData(json);
     } catch (error) {
       console.error(error);
@@ -55,19 +66,18 @@ const QuanLyHoaDon = () => {
         `http://${COMMON.ipv4}:3000/nhanviens/getNhanVien`,
       );
       const json = await response.json();
-      console.log(json);
       setNhanVienData(json);
     } catch (error) {
       console.error(error);
     }
   };
+
   const getHoaDon = async () => {
     try {
       const response = await fetch(
         `http://${COMMON.ipv4}:3000/hoadons/getHoaDon`,
       );
       const json = await response.json();
-      console.log(json);
       setData(json);
     } catch (error) {
       console.error(error);
@@ -75,10 +85,40 @@ const QuanLyHoaDon = () => {
       setLoading(false);
     }
   };
+
+  const addHoaDon = async () => {
+    let url_api_add = `http://${COMMON.ipv4}:3000/hoadons/addHoaDon/`;
+    let obj = {
+      idNhanVien: idNhanVien,
+      idKhachHang: idKhachHang,
+      ngayTao: getCurrentDate,
+      tongTien: tongTien,
+    };
+    fetch(url_api_add, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(obj),
+    })
+      .then(res => {
+        if (res.ok) {
+          Alert.alert('Thêm thành công');
+          setShowAddDialog(false);
+          getDanhSachDichVu();
+        }
+      })
+      .catch(err => {
+        console.log('Lỗi Thêm Nhân Viên', err);
+      });
+  };
+
   useEffect(() => {
     getHoaDon();
     getSpinnerKhachHang();
     getSpinnerNhanVien();
+    getCurrentDate();
   }, []);
 
   return (
@@ -120,27 +160,37 @@ const QuanLyHoaDon = () => {
         contentStyle={styles.fabContent}
       />
       <Modal visible={showAddDialog} transparent={true} animationType="slide">
-        <View style={styles.khungDialog}>
-          <View style={styles.dialog}>
-            <Text>Đây là Dialog mở đầu</Text>
+        <View style={styles.modalBackground}>
+          <View style={styles.modalContainer}>
+            <Text style={styles.modalTitle}>Tạo Hóa Đơn</Text>
+            <TextInput style={styles.input} placeholder="Nhập tên sản phẩm" />
+            <Text style={styles.info}>Ngày tạo: {currentDate}</Text>
             <Picker
               selectedValue={nhanVienHoTen}
-              onValueChange={(itemValue, itemIndex) =>
-                setNhanVienHoTen(itemValue)
-              }>
+              onValueChange={(itemValue, itemIndex) => {
+                setNhanVienHoTen(itemValue);
+                const selectedNhanVien = nhanVienData.find(
+                  nhanVien => nhanVien.hoTen === itemValue,
+                );
+                if (selectedNhanVien) {
+                  setIdNhanVien(selectedNhanVien.id);
+                }
+              }}>
               {nhanVienData.map((nhanVien, index) => (
                 <Picker.Item
                   key={index}
                   label={nhanVien.hoTen}
-                  value={nhanVien.id}
+                  value={nhanVien.hoTen} 
                 />
               ))}
             </Picker>
+
             <Picker
               selectedValue={khachHangHoTen}
-              onValueChange={(itemValue, itemIndex) =>
-                setKhachHangHoTen(itemValue)
-              }>
+              onValueChange={(itemValue, itemIndex) => {
+                setKhachHangHoTen(itemValue);
+                setIdKhachHang(itemValue);
+              }}>
               {khachHangData.map((khachHang, index) => (
                 <Picker.Item
                   key={index}
@@ -149,10 +199,10 @@ const QuanLyHoaDon = () => {
                 />
               ))}
             </Picker>
-            <Button
-              title="Đóng dialog"
-              onPress={() => setShowAddDialog(false)}
-            />
+            <TextInput style={styles.input} placeholder="Nhập giá tiền" />
+            <View style={styles.buttonContainer}>
+              <Button title="Đóng" onPress={() => setShowAddDialog(false)} />
+            </View>
           </View>
         </View>
       </Modal>
@@ -169,9 +219,8 @@ const styles = StyleSheet.create({
   },
   itemContainer: {
     backgroundColor: '#f9f9f9',
-    padding: 10,
-    marginVertical: 5,
-    marginHorizontal: 10,
+    padding: 20,
+    marginBottom: 10,
     borderRadius: 8,
     borderWidth: 1,
     borderColor: '#ddd',
@@ -179,38 +228,53 @@ const styles = StyleSheet.create({
   label: {
     fontWeight: 'bold',
     marginBottom: 5,
+    fontSize: 16,
   },
   info: {
-    marginBottom: 3,
-  },
-  emptyText: {
-    alignSelf: 'center',
-    marginTop: 20,
-    fontSize: 16,
-    color: '#888',
+    marginBottom: 8,
+    fontSize: 14,
   },
   fabStyle: {
     position: 'absolute',
+    margin: 16,
+    right: 0,
+    bottom: 0,
   },
   fabLabel: {
     fontSize: 16,
+    fontWeight: 'bold',
   },
   fabContent: {
-    paddingHorizontal: 8,
-    paddingVertical: 4,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
   },
-  khungDialog: {
+  modalBackground: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'rgba(0, 0, 0, 0.6)',
   },
-  dialog: {
-    width: 300,
-    height: 200,
-    backgroundColor: 'yellow',
-    padding: 10,
+  modalContainer: {
+    width: '80%',
+    backgroundColor: 'white',
+    padding: 20,
     borderRadius: 10,
-    opacity: 1,
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+    fontSize: 16,
+  },
+  buttonContainer: {
+    marginTop: 10,
   },
 });
